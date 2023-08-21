b4289d047661309a346b9137ec8511ff933fc354
Scot Curry <scotcurry4@gmail.com>
2023-08-21 15:25:02 -0400

    Hadn't exposed the port in the container

--- rssfeedpuller/rssfeederbuild.sh
+++ rssfeedpuller/rssfeederbuild.sh
@@ -1,5 +1,4 @@
 #! /bin/zsh
 
 cd ${HOME}/Projects/RSSFeedPuller/RSSFeedPuller
-read -nl
 docker build --file ${HOME}/K8sManifests/Dockerfile-rssfeedpuller-k8s --platform linux/amd64 --tag docker.io/scotcurry4/rssfeedpuller:${CURRENT_VERSION} .
